e8f2488d058db6590abea951f7bd20dafe47f689
donavynhaley@gmail.com <donavynhaley@gmail.com>
2020-09-20 13:03:57 -0700

    Implemented axios with authentication and tested backedn login/register end points

--- plantsapp/src/components/Home.js
+++ plantsapp/src/components/Home.js
@@ -1,7 +1,28 @@
-import React from "react";
-
-const Home = () => {
+import React, { useEffect } from "react";
+import { axiosWithAuth } from "./utils/axiosWithAuth";
+const Home = (props) => {
   // Code
+  const user = {
+    username: "donavyn",
+    phonenumber: "(123)456-7891",
+    email: "donavynhaley@gmail.com",
+    password: "hunter1",
+    first_name: "donavyn",
+    last_name: "haley",
+    experience: "some",
+    level: "1",
+  };
+  const userLogin = {
+    username: "donavyn",
+    password: "hunter1",
+  };
+  useEffect(() => {
+    axiosWithAuth()
+      .post("/login", userLogin)
+      .then((res) => {
+        console.log(res);
+      });
+  });
   return <div>Home</div>;
 };
 
