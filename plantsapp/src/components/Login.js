89f632699880eb3b03b5cf0fc417d8cc82193349
Melissa Tanksley <melissa.tanksley@gmail.com>
2020-09-22 15:39:03 -0700

    Yup and handles added

--- plantsapp/src/components/Login.js
+++ plantsapp/src/components/Login.js
@@ -1,6 +1,9 @@
-import React, { useState, Component } from "react";
-import styled from "styled-components";
-//import Axios from 'axios'
+import React, { useState, Component } from "react"
+import { FormControl } from "bootstrap"
+//import $ from "jquery";
+import styled from "styled-components"
+import "../styles/index.css"
+import * as Yup from 'yup'
 
 const StyledDiv = styled.button`
   color: ${(pr) => pr.theme.blue};
@@ -8,66 +11,78 @@ const StyledDiv = styled.button`
   justify-content: center;
 `;
 
-/*axios
-      .post('https://waterplantsunit4.herokuapp.com/login')
-      .then(res => {
-        console.log("Form -> res", res.data)
-        setPost([...post, res.data])
-        setFormState({
-          username: '',
-          password: ''
-        })
-        setServerError(null)
-      })
-      .catch(err => {
-        setServerError("There is an error!")
-      }) */
+const formSchema = Yup.object().shape({
+    username: 
+      Yup.string()
+      .username("Must be a valid username.")
+      .required("Must include username."),
+    password: 
+      Yup.string()
+      .min(6, "Passwords must be at least 6 characters long.")
+      .required("Password is Required.")
 
-export default class Login extends Component {
-  render() {
-    return (
-      <form>
-        <h3>Welcome Back!</h3>
+      // required isn't required for checkboxes.
+  });
 
-        <div className="form-group">
+  function Login() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+        function performValidation() {
+            return username.length > 0 && password.length > 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <form>
+      <h3>Sign In</h3>
+
+      <div className="form-group">
           <label>Username</label>
-          <input
-            type="username"
-            className="form-control"
-            placeholder="Enter username"
+          <input type="email" className="form-control" placeholder="Enter username" />
+      </div>
+          <FormControl
+              autoFocus
+                  type="text"
+                  value={username}
+                  onChange={e => setUsername(e.target.value)}
           />
-        </div>
-
-        <div className="form-group">
+      <div className="form-group">
           <label>Password</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Enter password"
+          <input type="password" className="form-control" placeholder="Enter password" />
+          <FormControl
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+              type="password"
           />
-        </div>
+      </div>
 
-        <div className="form-group">
+      <div className="form-group">
           <div className="custom-control custom-checkbox">
-            <input
-              type="checkbox"
-              className="custom-control-input"
-              id="customCheck1"
-            />
-            <label className="custom-control-label" htmlFor="customCheck1">
-              Remember me
-            </label>
+            <input type="checkbox"    className="custom-control-input" id="customCheck1" />
+            <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
           </div>
-        </div>
-        <StyledDiv>
+      </div>
+
+      <StyledDiv>
           <button type="submit" className="btn btn-primary btn-block">
-            Sign In<a href="https://amazing-mestorf-988b56.netlify.app/">.</a>
+            Sign In<a href="https://waterplantsunit4.herokuapp.com/login">.</a>
           </button>
-        </StyledDiv>
-        <p className="forgot-password text-right">
-          Forgot <a href="#">password?</a>
-        </p>
-      </form>
+      </StyledDiv>
+
+            <p className="forgot-password text-right">
+                Forgot <a href="#">password?</a>
+            </p>
+    </form>
     );
   }
-}
+
+export default Login;
+
+    
+        
+        
+
+
--- plantsapp/src/components/Signup.js
+++ plantsapp/src/components/Signup.js
@@ -1,5 +1,6 @@
-import React, { useState, Component } from "react";
-import styled from "styled-components";
+import React, { useState } from "react"
+import styled from "styled-components"
+import * as Yup from 'yup'
 //import Axios from 'axios'
 
 const StyledDiv = styled.button`
@@ -7,6 +8,106 @@ const StyledDiv = styled.button`
   border-radius: 5px;
   justify-content: center;
 `;
+
+const formSchema = Yup.object().shape({
+  
+  firstName: Yup
+    .string()
+    .min(3, "First name must be at least 3 characters long.")
+    .required("First name is required."),
+  lastName: Yup
+    .string()
+    .min(3, "Last name must be at least 3 characters long.")
+    .required("Last name is required."),
+  email: Yup
+    .string()
+    .email("Must be a valid email address.")
+    .required("Must include email address."),
+  password: Yup
+    .string()
+    .min(6, "Passwords must be at least 6 characters long.")
+    .required("Password is Required"),
+  
+    // required isn't required for checkboxes.
+});
+
+
+function Signup() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [emailAddress, setEmailAddress] = useState("");
+  const [FormControl, setFormControl] = useState("");
+  const [password, setPassword] = useState("");
+    
+  function performValidation() {
+      return firstName.length > 0 && lastName.length > 0;
+          
+    }
+  function handleSubmit(event) {
+      event.preventDefault();
+    }
+
+  
+  return (
+    <form>
+       <h3>Sign Up</h3>
+
+          <div className="form-group">
+              <label>First name</label>
+              <input type="text" className="form-control" placeholder="First name" />
+              <FormControl
+                  value={firstName}
+                  onChange={e => setFirstName(e.target.value)}
+                  type="firstName"
+              />
+          </div>
+
+          <div className="form-group">
+              <label>Last name</label>
+              <input type="text" className="form-control" placeholder="Last name" />
+              <FormControl
+                  value={lastName}
+                  onChange={e => setLastName(e.target.value)}
+                  type="lastName"
+              />
+          </div>
+
+          <div className="form-group">
+              <label>Email address</label>
+              <input type="email" className="form-control" placeholder="Enter email" />
+                  <FormControl
+                      value={emailAddress}
+                      onChange={e => setEmailAddress(e.target.value)}
+                      type="emailAddress"
+                  />
+          </div>
+
+          <div className="form-group">
+              <label>Password</label>
+              <input type="password" className="form-control" placeholder="Enter password" />
+              <FormControl
+                  value={password}
+                  onChange={e => setPassword(e.target.value)}
+                  type="password"
+              />
+          </div>
+
+          <StyledDiv>
+            <button type="submit" className="btn btn-primary btn-block">
+              Sign Up<a href="https://waterplantsunit4.herokuapp.com/register">.</a>
+            </button>
+          </StyledDiv>
+              <p className="forgot-password text-right">
+                   Already registered <a href="#">sign in?</a>
+              </p>
+    </form>
+  );
+}
+
+export default Signup;
+
+
+/*
 
 /*axios
       .post('https://waterplantsunit4.herokuapp.com/register')
@@ -25,52 +126,3 @@ const StyledDiv = styled.button`
         setServerError("There is an error!")
       }) */
 
-export default class SignUp extends Component {
-  render() {
-    return (
-      <form>
-        <h3>Join Us!</h3>
-
-        <div className="form-group">
-          <label>First name</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="First name"
-          />
-        </div>
-
-        <div className="form-group">
-          <label>Last name</label>
-          <input type="text" className="form-control" placeholder="Last name" />
-        </div>
-
-        <div className="form-group">
-          <label>Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Enter email"
-          />
-        </div>
-
-        <div className="form-group">
-          <label>Password</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Enter password"
-          />
-        </div>
-        <StyledDiv>
-          <button type="submit" className="btn btn-primary btn-block">
-            Sign Up<a href="https://amazing-mestorf-988b56.netlify.app/">.</a>
-          </button>
-        </StyledDiv>
-        <p className="forgot-password text-right">
-          Already registered <a href="#">sign in?</a>
-        </p>
-      </form>
-    );
-  }
-}
