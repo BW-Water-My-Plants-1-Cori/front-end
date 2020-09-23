6758b7301c29fe700f763544567deb7830ed850b
Melissa Tanksley <melissa.tanksley@gmail.com>
2020-09-23 12:24:59 -0700

    handle and yup updates

--- plantsapp/src/components/schemaLogin.js
+++ plantsapp/src/components/schemaLogin.js
@@ -1,23 +1,23 @@
+/*
+
 //import React, { useState } from "react"
 import * as Yup from 'yup'
 
 //const [schemaLogin] = useState("");
 
-function schemaLogin(props) {
+/*function schemaLogin(props) {
     return schemaLogin;
-}
+} 
 
-const formSchema = Yup.object().shape({
+const schemaLogin = Yup.object().shape({
     username: 
       Yup.string()
       .username("Must be a valid username.")
       .required("Must include username."),
     password: 
-      Yup.string()
-      .min(6, "Passwords must be at least 6 characters long.")
-      .required("Password is Required.")
+      Yup.string().min(6, "Passwords must be at least 6 characters long.").required("Password is Required.")
 
       // required isn't required for checkboxes.
   });
 
-  export default schemaLogin; 
+  export default schemaLogin; */
